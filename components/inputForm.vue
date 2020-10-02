4f1210602098afca7c6ee42e8dd36779e589e956
n4r1tahhhh <ddryutaro@gmail.com>
2020-10-03 06:40:18 +0900

    フォームとカードのコンポーネント

---
+++ components/inputForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="12">
+      <v-card>
+        <v-card-title class="headline">
+          {{ title }}
+        </v-card-title>
+        <v-card-text>
+          <slot></slot>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn class="mr-4" @click="submit">submit</v-btn>
+          <v-btn @click="clear">clear</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    submit() {
+      this.$emit('submit')
+    },
+    clear() {
+      this.$emit('clear')
+    },
+  },
+}
+</script>
