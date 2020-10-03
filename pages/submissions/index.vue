d7cd3fffc34da13fc480ffb8689f79f68c3fbb3a
n4r1tahhhh <ryutaro5126@fuji.waseda.jp>
2020-10-04 07:33:47 +0900

    スコア提出&一覧

---
+++ pages/submissions/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <simpleCard title="All Submits">
+    <div v-for="s in $store.state.submission.data" :key="s.score">
+      {{ s.score }}
+    </div>
+  </simpleCard>
+</template>
+
+<script>
+import simpleCard from '~/components/simpleCard'
+
+export default {
+  components: { simpleCard },
+  beforeCreate() {
+    this.$store.dispatch('submission/fetchData')
+  },
+}
+</script>
---
+++ pages/submissions/new.vue
@@ -0,0 +1,37 @@
+<template>
+  <inputForm title="Submit score" @submit="submit" @clear="clear">
+    <form>
+      <v-text-field v-model="score" label="score" type="number"></v-text-field>
+    </form>
+  </inputForm>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+import inputForm from '~/components/inputForm'
+
+export default {
+  components: { inputForm },
+  data() {
+    return {
+      score: null,
+    }
+  },
+  methods: {
+    async submit() {
+      // set params
+      const params = new URLSearchParams()
+      params.append('score', this.score)
+      // post and redirect
+      const res = await axios.post('/submissions', params)
+      if (res.status === 201) {
+        this.clear()
+        this.$router.push('/submissions')
+      }
+    },
+    clear() {
+      this.score = null
+    },
+  },
+}
+</script>
